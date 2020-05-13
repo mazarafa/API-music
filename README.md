42867e64ca1c595f38c40828e0897158259ca5e6
mazarafa7 <arruda.rafa.ra@gmail.com>
2020-05-13 16:15:42 -0500

    bd corrigido com sucesso, pronto para codar

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 API com PHP, extremamente basico utilizando o xampp. O bd foi utilizando phpmyadmin para verificar rodas e destino dos dados.
+
+
+
+Destinos da URL --> http://localhost/API/rest/music/mostrar
+
--- dados.sql
+++ dados.sql
@@ -1,9 +1,17 @@
 
 --\ Nome do bd é vintage /--
 
-DROP TABLE IF EXISTS `music`;
+
 CREATE TABLE `music` (
-    IdMatricula INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     Genero VARCHAR(12) NOT NULL,
-    name-artista VARCHAR(50) NOT NULL,
+    artista VARCHAR(50) NOT NULL
 );
+
+
+-- inserindo os dados no phpmyadmin ---
+
+INSERT INTO `music`(`id`, `Genero`, `artista`) VALUES (1,'techno','Vintage Culture'),
+														        (2,'House','Alok'),
+                                                                (3,'Rock','Ramones');
+														
